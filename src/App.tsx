adaecdf6e3ae62399c8f24688c9f5b70479416b6
Carlton Shi <jshi77@syr.edu>
2026-05-06 02:12:30 -0400

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
 import { LuxuryLanding } from './components/LuxuryLanding'
+import { ResumePage } from './pages/Resume'
+import NotFound from './pages/NotFound'
 
 export default function App() {
-  return <LuxuryLanding />
+  return (
+    <Routes>
+      <Route path="/" element={<LuxuryLanding />} />
+      <Route path="/resume" element={<ResumePage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
 }
