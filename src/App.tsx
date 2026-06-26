192dc83a3ac27a38c1b499d2d0138479b87eb6be
Carlton Shi <jshi77@syr.edu>
2026-06-26 11:18:37 -0400

    Remove works pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { DragCursorEffect } from './components/DragCursorEffect'
 import { LuxuryLanding } from './components/LuxuryLanding'
-import { ProjectDetail } from './pages/ProjectDetail'
 import { ResumePage } from './pages/Resume'
-import { WorksPage } from './pages/Works'
 import NotFound from './pages/NotFound'
 
 export default function App() {
@@ -12,8 +10,6 @@ export default function App() {
       <DragCursorEffect />
       <Routes>
         <Route path="/" element={<LuxuryLanding />} />
-        <Route path="/works" element={<WorksPage />} />
-        <Route path="/works/:id" element={<ProjectDetail />} />
         <Route path="/resume" element={<ResumePage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
